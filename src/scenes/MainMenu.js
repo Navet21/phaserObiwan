83d6593ce173b3b31343b6d2bfb78e587fb478ce
Navet21 <pablosantanaftv@hotmail.com>
2025-01-29 20:18:34 +0000

    Arreglado error de Cors

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,17 +1,54 @@
 import { Scene } from 'phaser';
 
+
+
+
 export class MainMenu extends Scene
 {
     constructor ()
     {
         super('MainMenu');
-    }
+    
+
+        this.llamadaApi = () => {
+            console.log("Esta funcionando");
+            fetch('https://bicicletas.onrender.com/api/bicicletas')
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    console.log(data.length);
+        
+                    if (data) { // Verificar que haya datos
+                        data.bicicletas.forEach((bicicleta, index) => {
+                            this.add.text(800, 460 + (index * 50), // Posición en Y aumenta por cada bicicleta
+                                `🚲 ${bicicleta.modelo || "Modelo desconocido"}`, 
+                                {
+                                    fontFamily: 'Arial Black',
+                                    fontSize: '24px',
+                                    color: '#ffffff',
+                                    stroke: '#000000',
+                                    strokeThickness: 6,
+                                    align: 'center'
+                                }
+                            );
+                        });
+                    } else {
+                        console.error("No se encontraron bicicletas en la API.");
+                    }
+                })
+                .catch(error => console.error('Error al obtener las bicicletas:', error));
+        }
+        
+    
+}
+    
 
     create ()
     {
         this.add.image(512, 384, 'background');
 
         const obiWan = this.add.image(512, 300, 'obiwan');
+        obiWan.setInteractive();
 
         this.tweens.add({
             targets: obiWan,
@@ -24,10 +61,12 @@ export class MainMenu extends Scene
             align: 'center'
         }).setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
 
-            this.scene.start('Game');
+        obiWan.on('pointerdown', function ()
+        {
 
-        });
+            this.llamadaApi();
+
+        },this);
     }
 }
